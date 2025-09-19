e860a99c42b03a7b61a358fb4c70480985afe2ac
ZephyrAmmor <amorzephyr@gmail.com>
2025-09-19 16:52:42 +0500

    Create 1st week's problems set

---
+++ week1/coutValues.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+  int age = 20;
+  float marks = 979;
+  char grade = 'A';
+  string name = "Abdul Samad";
+
+  cout
+    <<"My name is "
+    <<name
+    <<'.'
+    <<" I'm "
+    <<age
+    <<" years old"
+    <<'.'
+    <<" I got "
+    <<marks
+    <<" marks out of 1100."
+    <<" My Grade is "
+    <<grade
+    <<'.'
+    <<'\n';
+}
