181730e05ff30fa8d45fd42a7f8969b58392c529
Zephyr Ammor <amorzephyr@gmail.com>
2025-10-04 12:11:49 +0500

    Add error checking to make sure we get a positive integer

--- week5/odd-sum.cpp
+++ week5/odd-sum.cpp
@@ -4,6 +4,10 @@ int main () {
   int N = 0, total = 0;
   std::cout << "Enter a Number: ";
   std::cin >> N;
+  while ( N < 0) {
+    std::cout << "Kindly Provide Positive Integer: ";
+    std::cin >> N;
+  }
   for(int i = 1; i <= N; i += 2) {
     total+= i;
   }
