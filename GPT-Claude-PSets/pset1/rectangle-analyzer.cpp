f950f93bca52789ef9cfa6a6ba780ffda698a658
Zephyr Ammor <amorzephyr@gmail.com>
2025-10-05 14:51:07 +0500

    Add upto 2 decimals precision

--- GPT-Claude-PSets/pset1/rectangle-analyzer.cpp
+++ GPT-Claude-PSets/pset1/rectangle-analyzer.cpp
@@ -2,6 +2,8 @@
 #include <iomanip>
 #include <math.h>
 
+#define FIXED_FLOAT(x) std::fixed <<std::setprecision(2)<<(x)
+
 int main() {
   double width, length;
   std::cout << "Enter Length: ";
@@ -12,9 +14,9 @@ int main() {
     std::cout << "Invalid Dimensions";
     return 1;
   }
-  std::cout << "Area: " << width * length << std::endl;
-  std::cout << "Perimeter: " << 2 * width + 2 * length << std::endl;
-  std::cout << "Diagonal: " << sqrt(width * width + length * length) << std::endl;
-  std::cout << "Aspect Ration: " << length / width << " : 1" << std::endl;
+  std::cout << "Area: " << FIXED_FLOAT(width * length) << std::endl;
+  std::cout << "Perimeter: " << FIXED_FLOAT(2 * width + 2 * length) << std::endl;
+  std::cout << "Diagonal: " << FIXED_FLOAT(sqrt(width * width + length * length)) << std::endl;
+  std::cout << "Aspect Ration: " << FIXED_FLOAT(length / width) << " : 1" << std::endl;
   return 0;
 }
