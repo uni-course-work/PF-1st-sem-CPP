c2e47deabc7ab89e8dbdecf317640d92bf9c4360
Zephyr Ammor <amorzephyr@gmail.com>
2025-10-05 17:01:27 +0500

    Create a custom multiple table between provided range and output sum of products as well

---
+++ GPT-Claude-PSets/pset3/multiple-table.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+  int number, min, max, total = 0, temp;
+  std::cout << "Enter Number: ";
+  std::cin >> number;
+  std::cout << "Enter Start: ";
+  std::cin >> min;
+  std::cout << "Enter End: ";
+  std::cin >> max;
+  
+  if (min > max) {
+    int tmp = min;
+    min = max;
+    max = tmp;
+  }
+  for (int i = min; i <= max; i ++) {
+    temp = number * i;
+    std::cout << number << " x " << i << " = " << temp << std::endl;
+    total += temp;
+  }
+  std::cout << "Sum of Products: " << total << std::endl;
+  return 0;
+}
