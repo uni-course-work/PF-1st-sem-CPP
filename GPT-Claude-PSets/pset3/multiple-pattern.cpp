52d748a18bc2b39308cc666d4d746dfdb68e8365
Zephyr Ammor <amorzephyr@gmail.com>
2025-10-05 17:11:17 +0500

    Create a program that prints right angle and inverted triangle of size N provided by user

---
+++ GPT-Claude-PSets/pset3/multiple-pattern.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+  int N;
+  do {
+    std::cout << "Enter a number between 3 and 10 to get shapes: ";
+    std::cin >> N;
+  }
+  while (N < 3 || N > 10);
+  // Right Angle Triangle
+  for (int i = 1; i <= N; i ++) {
+    for (int j = 1; j <= i; j++){
+      std::cout << "* ";
+    }
+    std::cout<<std::endl;
+  }
+  std::cout << "------------------" << std::endl;
+  // Inverted Right Angle Triangle
+  for (int i = 0; i <= N; i++){
+    for (int j = N - i; j > 0; j -- ) {
+      std::cout << "* ";
+    }
+    std::cout << std::endl;
+  }
+}
