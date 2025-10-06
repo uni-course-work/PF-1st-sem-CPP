a697855ba801627d46043b4e7fa7f408c4d998f1
Zephyr Ammor <amorzephyr@gmail.com>
2025-10-06 23:04:34 +0500

    Create a simple income tax program

---
+++ GPT-Claude-PSets/pset5/incomeTax.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main () {
+  double income, taxPercent, tax, netIncome;
+
+  std::cout << "Enter Income: ";
+  std::cin >> income;
+  if (income > 200000) {
+    taxPercent = 20;
+  } else if (income > 100000) {
+    taxPercent = 10;
+  } else if (income > 50000) {
+    taxPercent = 5;
+  } else {
+    taxPercent = 0;
+  }
+  tax = income * (taxPercent / 100);
+  netIncome = income - tax;
+  std::cout << "Tax: " << tax << std::endl;
+  std::cout << "Net Income: " << netIncome << std::endl;
+}
