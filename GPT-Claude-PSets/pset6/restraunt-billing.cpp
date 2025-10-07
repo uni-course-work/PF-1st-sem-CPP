46fd50f2b79c87f58bac9b86409d1dc896df6608
Zephyr Ammor <amorzephyr@gmail.com>
2025-10-07 17:10:44 +0500

    Enhance restaurant system by adding a total, exit and condition of asking quantity

--- GPT-Claude-PSets/pset6/restraunt-billing.cpp
+++ GPT-Claude-PSets/pset6/restraunt-billing.cpp
@@ -1,34 +1,48 @@
 #include <iostream>
 
 int main() {
-  int option, quantity, subtotal;
+  int option, quantity, subtotal, total = 0;
+  bool exit = false;
   int burgerPrice = 500;
   int pizzaPrice = 800;
   int friesPrice = 200;
-  std::cout << "1. Burger - " << burgerPrice << std::endl;
-  std::cout << "2. Pizza - " << pizzaPrice << std::endl;
-  std::cout << "3. Fries - " << friesPrice << std::endl;
-  std::cout << "4. Exit" << std::endl;
-  std::cout << "Select Item (1-4): ";
-  std::cin >> option;
   do {
-    std::cout << "Quantity: ";
-    std::cin >> quantity;
-  } while (quantity < 1);
-  
-  switch (option)
-  {
-  case 1:
-    subtotal = burgerPrice * quantity;
-    break;
-  case 2:
-    subtotal = pizzaPrice * quantity;
-    break;
-  case 3:
-    subtotal = friesPrice * quantity;
-  default:
-    std::cout << "Unkown Option" << std::endl;
-    break;
-  }
-  std::cout << "Subtotal: " << subtotal << std::endl;
+    std::cout << "1. Burger - " << burgerPrice << std::endl;
+    std::cout << "2. Pizza - " << pizzaPrice << std::endl;
+    std::cout << "3. Fries - " << friesPrice << std::endl;
+    std::cout << "4. Exit" << std::endl;
+    std::cout << "Select Item (1-4): ";
+    std::cin >> option;
+    if (option != 4) {
+      do {
+        std::cout << "Quantity: ";
+        std::cin >> quantity;
+      } while (quantity < 1);
+    }
+
+    switch (option)
+    {
+    case 1:
+      subtotal = burgerPrice * quantity;
+      total += subtotal;
+      break;
+    case 2:
+      subtotal = pizzaPrice * quantity;
+      total += subtotal;
+      break;
+    case 3:
+      subtotal = friesPrice * quantity;
+      total += subtotal;
+      break;
+    case 4:
+      exit = true;
+      break;
+    default:
+      std::cout << "Unkown Option" << std::endl;
+      break;
+    }
+    std::cout << "Subtotal: " << subtotal << std::endl;
+  } while (exit == false);
+  std::cout << "Total: " << total << std::endl;
+  return 0;
 }
