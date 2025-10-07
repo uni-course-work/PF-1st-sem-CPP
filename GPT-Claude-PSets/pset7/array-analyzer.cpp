d696e40a0d66ebecfaa0708605c70d461ceef0ad
Zephyr Ammor <amorzephyr@gmail.com>
2025-10-07 20:51:59 +0500

    Create number of array analyzer

---
+++ GPT-Claude-PSets/pset7/array-analyzer.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int main() {
+  int arrayLength, odds = 0, evens = 0, min = 1000, max = -1000, sum = 0, range, elem;
+  double average;
+  do {
+    std::cout << "Enter number of element: ";
+    std::cin >> arrayLength;
+  } while (arrayLength < 1);
+  int array [arrayLength];
+  std::cout << "Values Should be smaller than 1000.";
+  std::cout << "Values: ";
+  for (size_t i = 0; i < arrayLength; i ++) {
+    do {
+      std::cin >> elem;
+    } while (elem > 1000 || array[i] < -1000);
+    if (elem < min) {
+      min = elem;
+    }
+    if (elem > max) {
+      max = elem;
+    }
+    if (elem % 2 == 0) {
+      evens ++;
+    } else {
+      odds ++;
+    }
+    sum += elem;
+    array[i] = elem;
+  }
+  std::cout << "Min: " << min << ", Max: " << max << ", Range: " << max - min << std::endl;
+  std::cout << "Average: " << (double) sum / (double) arrayLength << std::endl;
+  std::cout << "Even: " << evens << ", Odds: " << odds << std::endl;
+  std::cout << "Sum: " << sum << std::endl;
+  return 0;
+}
