41bfc1d4749778eff16aa838025d789c538ae993
Zephyr Ammor <amorzephyr@gmail.com>
2025-10-07 14:02:14 +0500

    Add some formatting to outputs

--- GPT-Claude-PSets/pset6/unit-converter.cpp
+++ GPT-Claude-PSets/pset6/unit-converter.cpp
@@ -6,6 +6,7 @@ int main () {
   std::cout << "2. Length" << std::endl;
   std::cout << "3. Weight" << std::endl;
   std::cout << "4. Currency" << std::endl;
+  std::cout << "5. Exit" << std::endl;
   std::cout << "Select Conversion: ";
   std::cin >> option;
 
@@ -15,29 +16,32 @@ int main () {
       std::cout << "Enter Temprature in C: ";
       std::cin >> value;
       newValue = (9.0 / 5.0) * value + 32;
-      std::cout << newValue << std::endl;
+      std::cout << value << " C" << " = " << newValue << " F" << std::endl;
       break;
     }
     case 2: {
       std::cout << "Enter CM: ";
       std::cin >> value;
       newValue = value / 2.54;
-      std::cout << newValue << std::endl;
+      std::cout << value << " cm" << " = " << newValue << " inch"<< std::endl;
       break;
     }
     case 3: {
       std::cout << "Enter Kg: ";
       std::cin >> value;
       newValue = value * 2.20462;
-      std::cout << newValue << std::endl;
+      std::cout << value << " Kg" << " = " << newValue << " Lb" << std::endl;
       break;
     }
     case 4: {
       std::cout << "Enter Currency PKR: ";
       std::cin >> value;
       newValue = value / 281.25;
-      std::cout << newValue << std::endl;
+      std::cout << value << " PKR" << " = " << newValue << " USD"  << std::endl;
       break;
+    }
+    case 5: {
+      return 0;
     }
   }
 }
