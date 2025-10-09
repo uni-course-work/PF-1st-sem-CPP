0e3f473d8a86103021c9f1bfba6344e45bb2643e
Zephyr Ammor <amorzephyr@gmail.com>
2025-10-09 20:18:40 +0500

    Add size of array dynamics

--- GPT-Claude-PSets/pset7/sorting-visulizer.cpp
+++ GPT-Claude-PSets/pset7/sorting-visulizer.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 
 int main() {
-  int array[5], sortedArray[5], temp;
-  std::cout << "Enter 5 Numbers: ";
-  for (size_t i = 0; i < 5; i++) {
+  int sizeOfArray;
+  std::cout << "Enter number of Integers you want to input: ";
+  std::cin >> sizeOfArray;
+  int array[sizeOfArray], sortedArray[sizeOfArray], temp;
+  std::cout << "Enter " << sizeOfArray << " Numbers: ";
+  for (size_t i = 0; i < sizeOfArray; i++) {
     std::cin >> array[i];
   }
   
-  for (size_t i = 1; i < 5; i++) {
-    for (size_t j = 1; j < 5; j++) {
+  for (size_t i = 1; i < sizeOfArray; i++) {
+    for (size_t j = 1; j < sizeOfArray; j++) {
       if (array[j] < array[j-1]) {
         int temp = array[j - 1];
         array[j - 1] = array[j];
@@ -17,12 +20,12 @@ int main() {
     }
   }
   std::cout << "Ascending: ";
-  for (size_t i = 0; i < 5; i++) {
+  for (size_t i = 0; i < sizeOfArray; i++) {
     std::cout << array[i] << ' ';
   }
   std::cout << std::endl;
   std::cout << "Desending: ";
-  for (size_t i = 5; i > 0; i --) {
+  for (size_t i = sizeOfArray; i > 0; i --) {
     std::cout << array[i - 1] << ' ';
   }
   std::cout << std::endl;
