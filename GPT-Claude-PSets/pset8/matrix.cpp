3d5a120a3f9550704c055116a971b83f790a7d16
Zephyr Ammor <amorzephyr@gmail.com>
2025-10-10 21:30:29 +0500

    Allow user to enter dimensions, add error checking

--- GPT-Claude-PSets/pset8/matrix.cpp
+++ GPT-Claude-PSets/pset8/matrix.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
 
 int main () {
-  int aMatrix[3][3];
-  int bMatrix[3][3];
+  int rows, columns;
 
+  do {
+  std::cout << "Enter the Dimensions of Matrices: ";
+  std::cin >> rows >> columns;
+  } while (rows < 0 || rows > 10 || columns < 0 || columns > 10);
   // Take Matices A and B
-  
+
+  int aMatrix[rows][columns];
+  int bMatrix[rows][columns];
   std::cout << "Enter Matrix A:"<< std::endl;
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++) {
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < columns; j++) {
       std::cin >> aMatrix[i][j];
     }
   }
   std::cout << std::endl;
   std::cout << "Enter B:" << std::endl;
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++) {
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < columns; j++) {
       std::cin >> bMatrix[i][j];
     }
   }
   // Sum of matrix a and b
 
   std::cout << "A + B:" << std::endl;
-  for (int i = 0; i < 3; i ++) {
-    for (int j = 0; j < 3; j++) {
+  for (int i = 0; i < rows; i ++) {
+    for (int j = 0; j < columns; j++) {
       std::cout << aMatrix[i][j] + bMatrix[i][j] << ' ';
     }
     std::cout << std::endl;
@@ -31,8 +36,8 @@ int main () {
   // Difference of Matrix a and b
 
   std::cout << "A - B:" << std::endl;
-  for (int i = 0; i < 3; i ++) {
-    for (int j = 0; j < 3; j++) {
+  for (int i = 0; i < rows; i ++) {
+    for (int j = 0; j < columns; j++) {
       std::cout << aMatrix[i][j] - bMatrix[i][j] << ' ';
     }
     std::cout << std::endl;
@@ -40,10 +45,15 @@ int main () {
   std::cout << "Transpose of A:" << std::endl;
 
   // Transpose of A
-  for (int i = 0; i < 3; i ++) {
-    for (int j = 0; j < 3; j++) {
-      std::cout << aMatrix[j][i] << ' ';
-    }
+  if (rows == columns) {
+    for (int i = 0; i < rows; i ++) {
+      for (int j = 0; j < columns; j++) {
+        std::cout << aMatrix[j][i] << ' ';
+      }
     std::cout << std::endl;
+    }
+  } else {
+    std::cout << "Can't take Transpose of a Rectangular Matrix" << std::endl;
   }
+  return 0;
 }
