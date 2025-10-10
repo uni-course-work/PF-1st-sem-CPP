03e4260cf915b0c0332dac4da170bd4cfe463260
Zephyr Ammor <amorzephyr@gmail.com>
2025-10-10 21:58:32 +0500

    Create a sinple words, chars, vowels, consonants counter

---
+++ GPT-Claude-PSets/pset8/word-char-counter.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int main() {
+  std::string text;
+  int words = 1, characters = 0, vowels = 0, consonants = 0;
+  std::cout << "Enter Text: ";
+  do {
+    std::getline(std::cin, text);
+  } while (text.length() < 1);
+  
+  size_t textLength = text.length();
+  for (int i = 0; i < textLength; i++) {
+    char lowercaseChar = towlower(text[i]);
+    if (lowercaseChar == ' '){
+      words ++;
+    } else {
+      characters ++;
+    }
+
+    if ( lowercaseChar == 'a' || lowercaseChar == 'e' || lowercaseChar == 'i' || lowercaseChar == 'o' || lowercaseChar == 'u'){
+      vowels ++;
+    } else if (isalpha(lowercaseChar)){
+      consonants ++;
+    }
+  }
+  std::cout << "Words: " << words << std::endl;
+  std::cout << "Characters: " << characters << std::endl;
+  std::cout << "Vowels: " << vowels << std::endl;
+  std::cout << "Consonants: " << consonants << std::endl;
+  
+}
